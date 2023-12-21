59811a548efbad6fe764ec50dbaf27fdb833f0be
Lijun56 <zhu944@purdue.edu>
2023-12-21 14:36:23 -0500

    fuckingdone5

--- src/12.js
+++ src/12.js
@@ -8,11 +8,10 @@ import {
 import Main from './layouts/Main'; // fallback for lazy pages
 import './static/css/main.scss'; // All of our styles
 
-const { PUBLIC_URL } = process.env;
-
 // Every route - we lazy load so that each page can be chunked
 // NOTE that some of these chunks are very small. We should optimize
 // which pages are lazy loaded in the future.
+// const About = lazy(() => import('./pages/About'));
 const Contact = lazy(() => import('./pages/Contact'));
 const Index = lazy(() => import('./pages/Index'));
 const NotFound = lazy(() => import('./pages/NotFound'));
