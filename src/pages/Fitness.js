cf7dab858944f083cd8cd0dee7f638f20337f0d5
Lijun56 <zhu944@purdue.edu>
2024-02-12 18:10:24 -0500

    restore fitnesscantbetter

--- src/pages/Fitness.js
+++ src/pages/Fitness.js
@@ -1,12 +1,29 @@
-import { useEffect } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const FitnessRedirect = () => {
-  useEffect(() => {
-    // Redirect to the external URL immediately when the component mounts
-    window.location.href = 'https://fitness-coach.pages.dev';
-  }, []); // Empty dependency array to ensure this runs once on mount
+import Main from '../layouts/Main';
 
-  return null; // This component does not render anything
-};
+const Index = () => (
+  <Main
+    description={
+      "Lijun Zhu's personal website. New York based Stanford ICME graduate, "
+      + 'VP of Engineering at Smile Identity, co-founder of Arthena and Matroid, and YC Alumni.'
+    }
+  >
+    <article className="post" id="index">
+      <header>
+        <div className="title">
+          <h2>
+            <Link to="/fitness">Fitness</Link>
+          </h2>
+          <p>
+            Lijun&apos;s Fitness Notes, please click{' '}
+            <a href="https://fitness-coach.pages.dev">here</a>.
+          </p>
+        </div>
+      </header>
+    </article>
+  </Main>
+);
 
-export default FitnessRedirect;
+export default Index;
