8c6cff335bc2fca049ad9fad279d4c60e5b090b2
Lijun56 <zhu944@purdue.edu>
2024-02-12 16:54:10 -0500

    push

--- src/App.js
+++ src/App.js
@@ -24,12 +24,10 @@ const Publication = lazy(() => import('./pages/Research'));
 
 const FitnessRedirect = () => {
   const navigate = useNavigate();
-
   useEffect(() => {
     // Redirect to the external URL
-    window.location.href = 'https://fitness-coach.pages.dev/';
+    window.location.href = "https://fitness-coach.pages.dev/";
   }, [navigate]);
-
   return null; // Render nothing, as we are redirecting
 };
 
